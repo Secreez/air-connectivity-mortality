8f7fbd889303fafea1ad1dd0e1eb953289e828ac
Secreez <m.elixhauser98@gmail.com>
2025-06-26 09:43:26 +0200

    - added derived folder -> for tables / as manuscript doesnt ref between files - fixed ref error with bashfile run

--- R/00_load_libs.R
+++ R/00_load_libs.R
@@ -1,15 +1,17 @@
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
 load_dependencies <- function() {
   cran_packages <- c(
     "styler", "tidyverse", "here", "countrycode", "ggrepel", "scales", "boot", "ppcor"
   )
-
+  
   github_packages <- list(
     wpp2024 = "PPgp/wpp2024"
   )
-
+  
   cran_missing <- cran_packages[!(cran_packages %in% installed.packages()[, "Package"])]
   if (length(cran_missing)) install.packages(cran_missing)
-
+  
   for (pkg in names(github_packages)) {
     if (!requireNamespace(pkg, quietly = TRUE)) {
       if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
@@ -17,7 +19,7 @@ load_dependencies <- function() {
       devtools::install_github(github_packages[[pkg]])
     }
   }
-
+  
   invisible(
     suppressPackageStartupMessages({
       lapply(c(cran_packages, names(github_packages)), library, character.only = TRUE)
