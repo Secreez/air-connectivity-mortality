ba716783f9a1cdfea5e910d7320557650ce235f0
Secreez <m.elixhauser98@gmail.com>
2025-07-01 10:57:52 +0200

    - added n.s i nglossary - thoughtabout idea / strategy how to compare  eurocontrol and opensky data. but very likely.. i can jsut compare march the two and then showcase feb data from opensky.

---
+++ R/comparing_eurocontrol_w_opensky.R
@@ -0,0 +1,2 @@
+# [TODO HERE !! 30.06! - CREATE PLOT FOR APPENDIX! AND EXPAND!]
+# [!! Compare March together and February for Opensky only to showcase that even Opensky data couldn't have saved the gap.]
