1a6d0b495b88d0e44cde9b94089c053be6a8b9ce
Secreez <m.elixhauser98@gmail.com>
2025-07-25 16:02:06 +0200

    - expanded information created references. - rewrote issues with load libs

--- R/00_load_libs.R
+++ R/00_load_libs.R
@@ -2,14 +2,15 @@ options(repos = c(CRAN = "https://cloud.r-project.org"))
 
 load_dependencies <- function() {
   cran_packages <- c(
-    "styler", "tidyverse", "tidyr", "here", "countrycode", "ggrepel", "scales", "boot", "ppcor", "lubridate"
+    "styler", "tidyverse", "here", "countrycode",
+    "ggrepel", "scales", "boot", "ppcor", "lubridate", "data.table"
   )
 
   github_packages <- list(
     wpp2024 = "PPgp/wpp2024"
   )
 
-  cran_missing <- cran_packages[!(cran_packages %in% installed.packages()[, "Package"])]
+  cran_missing <- setdiff(cran_packages, rownames(installed.packages()))
   if (length(cran_missing)) install.packages(cran_missing)
 
   for (pkg in names(github_packages)) {
@@ -20,10 +21,13 @@ load_dependencies <- function() {
     }
   }
 
+  libs <- c(cran_packages, names(github_packages))
   invisible(
-    suppressPackageStartupMessages({
-      lapply(c(cran_packages, names(github_packages)), library, character.only = TRUE)
-    })
+    suppressWarnings(
+      suppressPackageStartupMessages(
+        lapply(libs, require, character.only = TRUE)
+      )
+    )
   )
 }
 
